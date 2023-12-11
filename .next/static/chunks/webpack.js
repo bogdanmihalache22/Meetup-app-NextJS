67d24129a2e30c723d9eca5bf2d3a2e13b3e3d56
Bogdan Mihalache <bogdan.mihalache@calosense.com>
2023-12-11 11:37:24 +0200

    Layout Import Global Style

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4c86b866b7a407757910";
+/******/ 	var hotCurrentHash = "7eeed3cf25874ff21514";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,30 @@
+import Layout from "../components/layout/Layout";
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_MEETUP = [{
-    id: 'm1',
-    title: 'A First Meetup',
-    image: 'https://commons.wikimedia.org/wiki/File:Panoramica_Firenze.jpg#/media/File:Panoramica_Firenze.jpg',
-    address: 'Some Adress 5, 1234 Some City',
-    description: 'This is a first meetup!'
-},
-{
-    id: 'm2',
-    title: 'A Second Meetup',
-    image: 'https://commons.wikimedia.org/wiki/File:Panoramica_Firenze.jpg#/media/File:Panoramica_Firenze.jpg',    
-    address: 'Some Adress 55, 1234 Some City',
-    description: 'This is a Second meetup!'
-}]
+const DUMMY_MEETUP = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://commons.wikimedia.org/wiki/File:Panoramica_Firenze.jpg#/media/File:Panoramica_Firenze.jpg",
+    address: "Some Adress 5, 1234 Some City",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://commons.wikimedia.org/wiki/File:Panoramica_Firenze.jpg#/media/File:Panoramica_Firenze.jpg",
+    address: "Some Adress 55, 1234 Some City",
+    description: "This is a Second meetup!",
+  },
+];
 
 function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUP} />;
+  return (
+      <MeetupList meetups={DUMMY_MEETUP} />
+    
+  );
 }
 
 export default HomePage;
